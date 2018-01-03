a18d4ca6916ca1a3c77fbdf3ec4bbe8509570d3f
liverbool <nukboon@gmail.com>
2018-01-04 01:00:05 +0700

    remove unused interface

--- src/Toro/Pay/src/Provider/ToroPayProviderInterface.php
+++ src/Toro/Pay/src/Provider/ToroPayProviderInterface.php
@@ -11,43 +11,6 @@ use Psr\Http\Message\ResponseInterface;
 interface ToroPayProviderInterface
 {
     /**
-     * Builds the authorization URL.
-     *
-     * @param  array $options
-     * @return string Authorization URL
-     */
-    public function getAuthorizationUrl(array $options = []);
-
-    /**
-     * Redirects the client for authorization.
-     *
-     * @param  array $options
-     * @param  callable|null $redirectHandler
-     * @return mixed
-     */
-    public function authorize(array $options = [], callable $redirectHandler = null);
-
-    /**
-     * Requests an access token using a specified grant and option set.
-     *
-     * @param  mixed $grant
-     * @param  array $options
-     * @return AccessToken
-     */
-    public function getAccessToken($grant, array $options = []);
-
-    /**
-     * Sends a request instance and returns a response instance.
-     *
-     * WARNING: This method does not attempt to catch exceptions caused by HTTP
-     * errors! It is recommended to wrap this method in a try/catch block.
-     *
-     * @param  RequestInterface $request
-     * @return ResponseInterface
-     */
-    public function getResponse(RequestInterface $request);
-
-    /**
      * Requests and returns the resource owner of given access token.
      *
      * @param  AccessToken $token
